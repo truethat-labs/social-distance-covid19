bdf4d8d044b2b864590a14d1bb9981e818b96072
Gautam Sawhney <gsawhney21@gmail.com>
2020-04-04 12:30:21 +0530

    Added Shop Model

---
+++ db/migrate/20200404065727_create_shops.rb
@@ -0,0 +1,21 @@
+class CreateShops < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shops do |t|
+      t.string :name
+      t.bigint :phone_number
+      t.integer :shop_type, default: 0
+      t.text :address1
+      t.text :address2
+      t.string :city
+      t.string :state
+      t.string :country
+      t.string :zipcode
+      t.integer :max_allowed
+      t.decimal :lat
+      t.decimal :long
+      t.boolean :open, default: false
+
+      t.timestamps
+    end
+  end
+end
