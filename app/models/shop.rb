ae1ec0e06453575eebb411d07aaf57c3ee870c94
Gautam Sawhney <gsawhney21@gmail.com>
2020-04-04 19:24:36 +0530

    Desing changes for login and Signup page

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,7 @@
 class Shop < ApplicationRecord
   belongs_to :user, optional: true
-
+  # validates :name, :max_allowed, :latitude, :longitude, presence: true
   geocoded_by lat: :latitude, lon: :longitude
   enum shop_type: { Grocery: 0, Medicine: 1, Other: 2}
+
 end
