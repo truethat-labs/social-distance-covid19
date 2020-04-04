c13f425e24ad91e9cda57706829365077a7a4beb
Praveen Kumar <spkprav@gmail.com>
2020-04-04 19:54:17 +0530

    auto load shop address finished

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -3,10 +3,8 @@ google.maps.event.addDomListener(window, 'load', initAutocomplete);
 var placeSearch, autocomplete;
 
 var componentForm = {
-  street_number: 'short_name',
-  route: 'long_name',
   locality: 'long_name',
-  administrative_area_level_1: 'short_name',
+  administrative_area_level_1: 'long_name',
   country: 'long_name',
   postal_code: 'short_name'
 };
@@ -26,14 +24,47 @@ function initAutocomplete() {
   // When the user selects an address from the drop-down, populate the
   // address fields in the form.
   autocomplete.addListener('place_changed', getSelectedLocation);
+
+  if (document.getElementById('shop_latitude')) {
+    autocomplete.addListener('place_changed', getPlaceData);
+  }
 }
 
 function getSelectedLocation() {
   var place = autocomplete.getPlace();
   var lat = place.geometry.location.lat(),
     lng = place.geometry.location.lng();
-  console.log(lat, lng);
+
+  var shop_lat = document.getElementById('shop_latitude');
+  var shop_lng = document.getElementById('shop_longitude');
+
+  if (shop_lat && shop_lng) {
+    shop_lat.value = lat;
+    shop_lng.value = lng;
+  }
+
   afterGettingLocation(lat, lng);
+}
+
+function getPlaceData() {
+  var place = autocomplete.getPlace();
+  console.log(place);
+
+  for (var component in componentForm) {
+    console.log(component);
+    document.getElementById(component).value = '';
+    document.getElementById(component).disabled = false;
+  }
+
+  // Get each component of the address from the place details,
+  // and then fill-in the corresponding field on the form.
+  for (var i = 0; i < place.address_components.length; i++) {
+    var addressType = place.address_components[i].types[0];
+    if (componentForm[addressType]) {
+      var val = place.address_components[i][componentForm[addressType]];
+      document.getElementById(addressType).value = val;
+    }
+  }
 }
 
 $(document).on('click', '#getLocation', function() {
@@ -56,7 +87,9 @@ $(document).on('click', '#getLocation', function() {
 });
 
 function afterGettingLocation(lat, long) {
-  $.get("/nearby?lat="+lat+"&long="+long, function (data, status) {
-    document.getElementById('nearby_stores').innerHTML = data;
-  });
+  if(document.getElementById('nearby_stores')) {
+    $.get("/nearby?lat="+lat+"&long="+long, function (data, status) {
+      document.getElementById('nearby_stores').innerHTML = data;
+    });
+  }
 }
