9f6f014b3f45274b5412ecbe5fb6f76a4237406a
Gautam Sawhney <gsawhney21@gmail.com>
2020-04-04 15:41:41 +0530

    Added new action for shops

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,5 +5,23 @@ class ShopsController < ApplicationController
   end
 
   def create
+    @shop = Shop.new(shop_params)
+    @shop.save
+
+    if @shop.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @shop = Shop.find(params[:id])
+  end
+
+  private
+
+    def shop_params
+      params.require(:shop).permit(:name, :phone_number, :shop_type, :address1, :address2, :city, :state, :country, :zipcode, :max_allowed, :lat, :long, :open)
+    end
 end
